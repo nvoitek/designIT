9762e811a15d80be088041fed2547cfdd4fc73a5
nvoitek <nvoitek@gmail.com>
2021-08-04 20:07:31 +0200

    Menu toggle

--- header.php
+++ header.php
@@ -51,7 +51,7 @@
 					</div><!-- .site-branding -->
 
 					<nav id="site-navigation" class="main-navigation">
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'designit'); ?></button>
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo "<i class='fas fa-bars'></i>"?></button>
 						<?php
 						wp_nav_menu(
 							array(
